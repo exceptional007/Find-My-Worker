88e4249622c4b6f90283a76ac99cf956459c4950
exceptional007 <akshhatsri7843@gmail.com>
2025-07-31 15:29:11 +0530

    Database Deploy Render 1

--- Find_My_Worker/settings.py
+++ Find_My_Worker/settings.py
@@ -80,12 +80,12 @@ WSGI_APPLICATION = 'Find_My_Worker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 import dj_database_url
 
